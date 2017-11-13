1d8c71ba66633a15e18c1367ad6b0671884197fe
nathanbrachotte <Tillou77>
2017-11-13 15:24:51 +0100

    Base

---
+++ components/About.js
@@ -0,0 +1,17 @@
+/**
+ * Created by nathanbrachotte on 13/11/17.
+ */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class About extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Open up y.js to start working on your app!</Text>
+                <Text>Changes you make will automatically reload.</Text>
+                <Text>Shake your phone to open the developer menu.</Text>
+            </View>
+        );
+    }
+}
